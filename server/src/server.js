c4a282f5483b84a06246d51d5a249d62f9138f0a
Vansham Aggarwal <vanshamagg@gmail.com>
2021-01-18 21:51:24 +0530

    added HTTPS

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,13 @@
 import 'dotenv/config';
-import './models'
+import './models';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import api from './routes/api';
+import 'colors';
+import https from 'https';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,6 +25,12 @@ app.get('/', (req, res) => {
   res.send(message);
 });
 
+const httpsOptions = {
+  key: readFileSync(resolve(__dirname, '..', 'security', 'key.pem')),
+  cert: readFileSync(resolve(__dirname, '..', 'security', 'cert.pem')),
+  passphrase: 'vansham',
+};
 
-
-const server = app.listen(PORT, (_) => console.log(`Server Started at ${server.address().port}`));
+const server = https
+  .createServer(httpsOptions, app)
+  .listen(PORT, (_) => console.log(`HTTPS server running at https://localhost:${PORT}`.inverse));
