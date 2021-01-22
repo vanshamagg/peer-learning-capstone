4f673d4521cc03f2963820c49a5e7818ecbcc829
basilmaben <basilmaben.bm@gmail.com>
2021-01-22 18:47:17 +0530

    added toggle

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import {SidebarContainer, Icon, CloseIcon, SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, SidebarRoute } from './SidebarElements';
-const Sidebar = () => {
+const Sidebar = ({isOpen , toggle}) => {
     return (
-        <SidebarContainer>
-            <Icon>
+        <SidebarContainer isOpen = {isOpen} onClick = {toggle}>
+            <Icon onClick= {toggle}>
                 <CloseIcon/>
             </Icon>
             <SidebarWrapper>
                 <SidebarMenu>
-                    <SidebarLink to = 'about'>
+                    <SidebarLink to = 'about' onClick = {toggle}>
                         About
                     </SidebarLink>
-                    <SidebarLink to = 'discover'>
+                    <SidebarLink to = 'discover' onClick = {toggle}>
                         Discover
                     </SidebarLink>
-                    <SidebarLink to = 'services'>
+                    <SidebarLink to = 'services' onClick = {toggle}>
                         Services
                     </SidebarLink>
-                    <SidebarLink to = 'signup'>
+                    <SidebarLink to = 'signup' onClick = {toggle}>
                         Sign Up
                     </SidebarLink>
                 </SidebarMenu>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
 
 const Home = () => {
+
+const [isOpen, setisOpen] = useState(false)
+const toggle =()=> {
+    setisOpen(!isOpen)
+}
+
     return (
         <>
-            <Sidebar/>
-            <Navbar/>
+            <Sidebar isOpen = {isOpen} toggle = {toggle}/>
+            <Navbar toggle = {toggle}/>
         </>
     )
 }
