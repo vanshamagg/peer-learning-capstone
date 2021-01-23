e6daa531f935c092ea8d4bcfb1b5cfa975a74739
basilmaben <basilmaben.bm@gmail.com>
2021-01-23 17:05:19 +0530

    effects added

--- client/src/components/HeroSection/index.js
+++ client/src/components/HeroSection/index.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Video from '../../videos/video.mp4'
-import {HeroContainer, HeroBg, VideoBg} from './HeroElements'
+import {HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowRight, ArrowForward} from './HeroElements'
+import {Button} from '../ButtonElement'
 
 const HeroSection = () => {
+    const [hover,setHover] = useState(false)
+
+    const onHover =()=>{
+        setHover(!hover)
+    }
+
     return (
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoplay loop muted src={Video} type='video/mp4'/>
             </HeroBg>
+            <HeroContent>
+                <HeroH1>
+                    Peer Learning made easy
+                </HeroH1>
+                <HeroP>
+                    Sign up and connect with students from everywhere
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to ='signup' onMouseEnter = {onHover} onMouseLeave = {onHover} >
+                        Get started{hover ? <ArrowForward/> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
     )
 }
